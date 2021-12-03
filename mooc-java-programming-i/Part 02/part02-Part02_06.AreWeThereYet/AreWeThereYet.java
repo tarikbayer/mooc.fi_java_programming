a626b6a32f4b96231d35851e1bcb93b244a2778e
tarık bayer <31344520+tarikbayer@users.noreply.github.com>
2021-12-03 13:07:03 +0100

    Create AreWeThereYet.java

---
+++ mooc-java-programming-i/Part 02/part02-Part02_06.AreWeThereYet/AreWeThereYet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class AreWeThereYet {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = scanner.nextInt();
+            if (number == 4) {
+                break;
+            }
+        } 
+ 
+    }
+}
+ 
