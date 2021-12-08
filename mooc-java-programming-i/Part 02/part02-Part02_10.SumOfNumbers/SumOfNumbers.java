115f2abb2ca2c26c5900ad7ed4f220fccb2662e0
tarık bayer <31344520+tarikbayer@users.noreply.github.com>
2021-12-08 20:21:55 +0100

    Create SumOfNumbers.java

---
+++ mooc-java-programming-i/Part 02/part02-Part02_10.SumOfNumbers/SumOfNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class SumOfNumbers {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        
+        while(true) {
+            System.out.println("Give a number:");
+            int number = scanner.nextInt();
+            
+            if (number == 0) {
+                break;
+            } else {
+                count = count + number;
+            }
+            
+        }
+        System.out.println("Sum of the numbers: " + count);
+ 
+    }
+}
