3394810234200a6818c476e908531ea64fc09a88
tarık bayer <31344520+tarikbayer@users.noreply.github.com>
2022-01-03 17:57:48 +0100

    Create Counting.java

---
+++ mooc-java-programming-i/Part 02/part02-Part02_14.Counting/Counting.java
@@ -0,0 +1,18 @@
+ 
+import java.util.Scanner;
+ 
+public class Counting {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int count = scanner.nextInt();
+        
+        
+        for (int i = 0; i <= count; i++) {
+            System.out.println(i);
+        }
+ 
+    }
+}
+ 
